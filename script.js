57cad96ad55dbe951d7079eae2f0a491187f016d
YashVishnoi47 <vikasvishnoi005@gmail.com>
2024-06-25 13:30:37 +0530

    basic javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("script is working")
+
+
+
+async function getsongs(){
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text()
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith("mp3")){
+            songs.push(element.href)
+        }
+        
+    }
+    return songs
+}
+
+let songs  = getsongs();
+console.log(songs)
