eccdccacdc68240b4b015c6f15b3344df413ea88
YashVishnoi47 <vikasvishnoi005@gmail.com>
2024-06-27 17:54:14 +0530

    basic javascript logic update and basic playbar html

--- script.js
+++ script.js
@@ -12,12 +12,40 @@ async function getsongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith("mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
+            
         }
         
     }
     return songs
 }
 
-let songs  = getsongs();
-console.log(songs)
+async function main(){
+    let songs  = await getsongs();
+    console.log(songs)
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><div class="songlist-li-songinfo">
+
+                                <i class="fa-sharp fa-solid fa-music"></i>
+
+                                <div class="songinfo">
+                                    <p class="song-name">${song.replaceAll("%20", " ")} </p>
+                                    <p class="song-artist">Yash</p>
+                                </div>
+
+                            </div>
+
+                            <span> Play<i class="fa-solid fa-play"></i></span></li>`
+
+    }
+
+    var audio = new Audio(songs[0]); 
+    // audio.play(); 
+}
+ 
+main()
+
+
+
